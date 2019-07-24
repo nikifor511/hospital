40e36e61e17e7a7107e4eb2bd2912ef4499cbd50
nikifor511 <deadnikifor@gmail.com>
2019-07-24 17:59:28 +0300

    e

--- src/sample/ControllerLoginWindow.java
+++ src/sample/ControllerLoginWindow.java
@@ -1,9 +1,15 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -15,18 +21,30 @@ public class ControllerLoginWindow {
     private TextField login_text_field;
 
     @FXML
+    private PasswordField pass_field;
+
+    @FXML
     public void onCancelButtonMethod(){
         Stage stage = (Stage) login_text_field.getScene().getWindow();
         stage.close();
     }
 
     @FXML
-    public void onLoginButtonMethod() throws SQLException {
-        ResultSet res = myDB.query("select login_user('login1', 'pass2')");
+    public void onLoginButtonMethod() throws SQLException, IOException {
+        String sql_str = "select login_user('";
+        sql_str = sql_str + login_text_field.getText() + "', '" + pass_field
+        ResultSet res = myDB.query("select login_user('login1', 'pass1')");
         res.next();
         if (res.getInt("login_user") == 0) {
+            Parent root = FXMLLoader.load(getClass().getResource("hospital_window.fxml"));
+            Stage HospitalStage = new Stage();
+            HospitalStage.setTitle("Hospital");
+            HospitalStage.initModality(Modality.APPLICATION_MODAL);
+            HospitalStage.setScene(new Scene(root, 600, 400));
+            HospitalStage.show();
 
-
+            Stage stage = (Stage) login_text_field.getScene().getWindow();
+            stage.close();
         }
         System.out.println(res.getInt("login_user"));
     }
